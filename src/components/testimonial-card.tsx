d08fe9660026b44dc914c26e1f199db455b30b10
Derek Santolo <derek.santolo@gmail.com>
2025-12-16 12:54:47 -0600

    feat: make component height responsive

--- src/components/testimonial-card.tsx
+++ src/components/testimonial-card.tsx
@@ -11,19 +11,23 @@ export default function TestimonialCard({
   testimonial = "",
 }: TestimonialCardProps) {
   return (
-    <div className="flex h-[233px] w-[340px] flex-col gap-4 rounded-lg bg-white p-6">
-      <div className="flex h-[49px] w-[292px] gap-4">
+    <div className="flex min-h-[233px] w-[340px] flex-col gap-4 rounded-lg bg-white p-6">
+      <div className="flex min-h-[49px] w-[292px] gap-4">
         <img
           className="size-12 rounded-full"
           src={image.src}
           alt={`Photo of ${name}`}
         />
         <div className="flex flex-col">
-          <div className="text-lg font-semibold text-neutral-900">{name}</div>
+          <div className="text-lg font-semibold wrap-anywhere text-neutral-900">
+            {name}
+          </div>
           <div className="text-sm text-neutral-600">{handle}</div>
         </div>
       </div>
-      <div className="text-base text-neutral-600">{testimonial}</div>
+      <div className="line-clamp-5 h-full text-base text-neutral-600">
+        {testimonial}
+      </div>
     </div>
   );
 }
